d024293ed1befc85d91b54f2faa34223ad76bf9a
Christian Gerber <cgerber3@illinois.edu>
2022-11-23 22:38:39 -0500

    Updated sentiment analysis to use sentence tokenization

--- Project/ScratchPad.py
+++ Project/ScratchPad.py
@@ -1,10 +1,19 @@
 from pathlib import Path
-
+import numpy as np
 import praw
 import config
 import pprint
 import psaw
 import logging
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.tokenize import sent_tokenize
+from GetPlayerProjectedPoints import get_player_stats
+import pandas as pd
+import datetime as dt
+
+nltk.download(["vader_lexicon", "punkt"])
+sia = SentimentIntensityAnalyzer()
 pp = pprint.PrettyPrinter(indent=4)
 # Define user agent
 user_agent = "praw_scraper_1.0"
@@ -17,20 +26,14 @@ reddit = praw.Reddit(username=config.username,
                      user_agent=user_agent)
 api = psaw.PushshiftAPI(reddit)
 
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
-from GetPlayerProjectedPoints import get_player_stats
-import pandas as pd
-import datetime as dt
-nltk.download(["vader_lexicon"])
-sia = SentimentIntensityAnalyzer()
+
 
 runningSum = 0
 commentsViewed = 0
 maxComments = 1000000
 players = get_player_stats()
 players = players.sort_values('Projected Points', ascending=False)
-players = players.head(250)
+players = players.head(10)
 players = players.set_index('Full Name')
 
 start_epoch=int(dt.datetime(2022, 10, 23).timestamp())
@@ -43,17 +46,21 @@ logger = logging.getLogger('psaw')
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
+
 for name, row in players.iterrows():
-    for comment in api.search_comments(q=name, subreddit='fantasyfootball', limit=1000, after=start_epoch, before=end_epoch):
-        score = sia.polarity_scores(comment.body)['compound']
-        if score < players.loc[name, "Most Negative Comment Score"]:
-            players.loc[name, "Most Negative Comment Score"] = score
-            players.loc[name, "Most Negative Comment"] = comment.body
-        if score > players.loc[name, "Most Positive Comment Score"]:
-            players.loc[name, "Most Positive Comment Score"] = score
-            players.loc[name, "Most Positive Comment"] = comment.body
-        players.loc[name, "Sentiment"] += score
-        players.loc[name, "NumOfDataPoints"] += 1
+    for comment in api.search_comments(q=name, subreddit='nfl', limit=1000, after=start_epoch, before=end_epoch):
+        for paragraph in comment.body.split('\n'):
+            if name in paragraph:
+                for sentence in sent_tokenize(paragraph):
+                    score = sia.polarity_scores(sentence)['compound']
+                    if score < players.loc[name, "Most Negative Comment Score"]:
+                        players.loc[name, "Most Negative Comment Score"] = score
+                        players.loc[name, "Most Negative Comment"] = sentence
+                    if score > players.loc[name, "Most Positive Comment Score"]:
+                        players.loc[name, "Most Positive Comment Score"] = score
+                        players.loc[name, "Most Positive Comment"] = sentence
+                    players.loc[name, "Sentiment"] += score
+                    players.loc[name, "NumOfDataPoints"] += 1
 
 
 for name, row in players.iterrows():
--- Project/SentimentAnalysis.py
+++ Project/SentimentAnalysis.py
@@ -2,5 +2,14 @@ import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 nltk.download(["vader_lexicon"])
 sia = SentimentIntensityAnalyzer()
-score = sia.polarity_scores("This is a great test phrase!")
-print(score['compound'])
+
+print(sub_toks)
+
+score = sia.polarity_scores("Delusional reddit haters will see just how bad this Cardinals offence is without Kyler Murray.")
+print(score)
+score = sia.polarity_scores("They can't run for shit so I won't knock them for throwing it a lot (especially since one was a long OT game) but how the fuck do you let Justin Herbert throw it damn near 60 times and end up with such low yardage total")
+print(score)
+#score = sia.polarity_scores("Josh Allen sucks, Patrick Mahomes is great!")
+#print(score['compound'])
+#score = sia.polarity_scores("Patrick Mahomes sucks")
+#print(score['compound'])
